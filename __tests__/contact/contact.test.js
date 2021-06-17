018d73fbf2573825b3471cdd1f746314673ae93d
Hugo Lachkar <hugo1.lachkar@epitech.eu>
2021-06-17 17:16:21 +0200

    [ADD] Contact unit test

---
+++ __tests__/contact/contact.test.js
@@ -0,0 +1,15 @@
+import React from "react"
+import renderer from "react-test-renderer"
+
+import Contact from "../../src/components/Contact"
+
+describe("Contact", () => {
+  it("renders correctly", async () => {
+    const datas = require("../../static/pages/pages.en.json");
+
+    const tree = renderer
+      .create(<Contact datas={datas.content.contact} />)
+      .toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+})
