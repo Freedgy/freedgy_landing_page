9d645bc0ca3b56317c5af0175b307e7c08533198
Hugo Lachkar <hugo1.lachkar@epitech.eu>
2021-06-17 17:16:38 +0200

    [ADD] Footer unit test

---
+++ __tests__/footer/footer.test.js
@@ -0,0 +1,15 @@
+import React from "react"
+import renderer from "react-test-renderer"
+
+import Footer from "../../src/components/Footer"
+
+describe("Footer", () => {
+  it("renders correctly", async () => {
+    const datas = require("../../static/pages/pages.en.json");
+
+    const tree = renderer
+      .create(<Footer datas={datas.layout.footer} />)
+      .toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+})
