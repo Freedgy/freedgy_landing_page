66def5c72427e603f48ffc1d1164931e9c494000
Hugo Lachkar <hugo1.lachkar@epitech.eu>
2021-06-17 17:16:29 +0200

    [ADD] Context unit test

---
+++ __tests__/context/context.test.js
@@ -0,0 +1,15 @@
+import React from "react"
+import renderer from "react-test-renderer"
+
+import Context from "../../src/components/Context"
+
+describe("Context", () => {
+  it("renders correctly", async () => {
+    const datas = require("../../static/pages/pages.en.json");
+
+    const tree = renderer
+      .create(<Context datas={datas.content.context} />)
+      .toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+})
