158e6ea35592fb0f071a2d6fdf9eb4d63a8acf18
Hugo Lachkar <hugo1.lachkar@epitech.eu>
2021-06-17 17:39:43 +0200

    [ADD] team -> unit test

---
+++ __tests__/project/project.test.js
@@ -0,0 +1,15 @@
+import React from "react"
+import renderer from "react-test-renderer"
+
+import Project from "../../src/components/Project"
+
+describe("Project", () => {
+  it("renders correctly", async () => {
+    const datas = require("../../static/pages/pages.en.json");
+
+    const tree = renderer
+      .create(<Project datas={datas.content.project} />)
+      .toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+})
